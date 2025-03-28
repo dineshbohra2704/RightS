7d2495183c8b7722880d7cd018dc2155dee50ebc
Rajesh Singh Bohra <siinghraajesh@gmail.com>
2025-03-28 10:59:46 +0530

    1

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  allowedDevOrigins: ["http://192.168.0.102"], // Add your dev origin here
+};
+
+module.exports = nextConfig;
